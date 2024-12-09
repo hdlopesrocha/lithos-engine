30d44e6c6b3fc2649194e4d2c8b8f3151c4d063e
Henrique Rocha <hdlopesrocha@protonmail.com>
2024-12-09 18:59:58 +0000

    Add simple shaders

--- shaders/fragShader.glsl
+++ shaders/fragShader.glsl
@@ -1,26 +1,8 @@
-#version 450 core
-#extension GL_ARB_separate_shader_objects : enable
+#version 460 core
 
-layout(binding = 1) uniform sampler2D texSampler[16];
-
-layout(location = 0) in vec3 fragNormal;
-layout(location = 1) in vec2 fragTexCoord;
-layout(location = 2) in float fragTexIndex[16];
-
-layout(location = 0) out vec4 outColor;
+in vec3 fragColor; // Interpolated color from the vertex shader
+out vec4 color;    // Final fragment color
 
 void main() {
-	vec3 lightDir = vec3(0.0, 0.0, -1.0);
-	outColor = vec4(0.0);
-	
-	for(int i=0 ; i < 16; ++i) {
-    	if(fragTexIndex[i]>0){
-    		outColor += fragTexIndex[i]*texture(texSampler[i], fragTexCoord);
-    	}
-	}
-
-    outColor.xyz *= clamp(dot(-lightDir, fragNormal), 0.5,1.0);
-    if (outColor.w == 0.0)
-      discard; 
+    color = vec4(fragColor, 1.0); // Output color with alpha = 1.0
 }
-
--- shaders/vertShader.glsl
+++ shaders/vertShader.glsl
@@ -1,38 +1,20 @@
-#version 450 core
-#extension GL_ARB_separate_shader_objects : enable
+#version 460 core
 
-// Uniform attributes
-layout(binding = 0) uniform UniformBufferObject {
-    mat4 view;
-    mat4 projection;
-} ubo;
+// Input from the vertex array
+layout(location = 0) in vec3 position; // Vertex position (x, y, z)
+layout(location = 1) in vec3 color;    // Vertex color (r, g, b)
 
-// Vertex attributes
-layout(location = 0) in vec3 inPosition;
-layout(location = 1) in vec3 inNormal;
-layout(location = 2) in vec2 inTexCoord;
-layout(location = 3) in uint inTexIndex;
+// Output to the fragment shader
+out vec3 fragColor;
 
-// Instance attributes
-uniform PushConstants {
-	mat4 model;
-} pushConsts;
-
-
-layout(location = 0) out vec3 fragNormal;
-layout(location = 1) out vec2 fragTexCoord;`
-layout(location = 2) out float fragTexIndex[16];
-
-out gl_PerVertex {
-    vec4 gl_Position;
-};
+uniform mat4 model;      // Model transformation matrix
+uniform mat4 view;       // View transformation matrix
+uniform mat4 projection; // Projection transformation matrix
 
 void main() {
-    gl_Position = ubo.projection * ubo.view * pushConsts.model * vec4(inPosition, 1.0);
-    fragNormal = inNormal;
-    fragTexCoord = inTexCoord;
-    for(int i =0 ; i < 16 ; ++i) {
-		fragTexIndex[i] = i == inTexIndex ? 1.0: 0.0;
-    } 
-}
+    // Pass color to the fragment shader
+    fragColor = color;
 
+    // Calculate the final position of the vertex
+    gl_Position = projection * view * model * vec4(position, 1.0);
+}
