3ac48a0604423be911ecaa75bca7c38862d407ce
Henrique Rocha <hdlopesrocha@protonmail.com>
2025-02-13 15:48:19 +0000

    Using mat3 for 2d vegetation

--- gl/VegetationTexture.cpp
+++ gl/VegetationTexture.cpp
@@ -23,7 +23,7 @@ void VegetationTexture::mix(){
 
     glUseProgram(program);
 
-    glm::mat4 model = glm::rotate(glm::scale(glm::mat4(1.0), glm::vec3(0.5)), (float) (M_PI/4.0), glm::vec3(0.0,0.0,1.0) );
+    glm::mat3 model = glm::rotate(glm::scale(glm::mat4(1.0), glm::vec3(0.5)), (float) (M_PI/4.0), glm::vec3(0.0,0.0,1.0) );
 
     glBindFramebuffer(GL_DRAW_FRAMEBUFFER, textureBuffer.frameBuffer);
     glViewport(0, 0, textureBuffer.width, textureBuffer.height);
@@ -31,7 +31,7 @@ void VegetationTexture::mix(){
     glActiveTexture(GL_TEXTURE0); 
     glBindTexture(GL_TEXTURE_2D_ARRAY, texture->texture);
     glUniform1i(this->samplerLoc, 0);
-	glUniformMatrix4fv(this->modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+	glUniformMatrix3fv(this->modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
     for (int layer = 0; layer < 3; ++layer) {
         glUniform1i(this->layerLoc, layer);
--- shaders/texture/vegetation_vertex.glsl
+++ shaders/texture/vegetation_vertex.glsl
@@ -3,9 +3,9 @@ layout(location = 0) in vec2 position;
 layout(location = 1) in vec2 texCoord;
 
 out vec2 vTexCoord;
-uniform mat4 model;
+uniform mat3 model;
 
 void main() {
     vTexCoord = texCoord;
-    gl_Position = model * vec4(position, 0.0, 1.0);
+    gl_Position = vec4(model * vec3(position, 0.0), 1.0);
 }
