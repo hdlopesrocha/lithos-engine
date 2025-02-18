469a10d8caf9da03435779e1aa29b848ade1102d
Henrique Rocha <hdlopesrocha@protonmail.com>
2025-02-18 16:48:12 +0000

    Blend multiple DrawTiles

--- shaders/texture/atlas_fragment.glsl
+++ shaders/texture/atlas_fragment.glsl
@@ -7,13 +7,15 @@ uniform sampler2DArray textureSampler;
 in flat int Layer;
 
 void main() {    
-
-    vec4 color = texture(textureSampler, vec3(TexCoord,Layer));
+    vec4 opacity = texture(textureSampler, vec3(TexCoord,2));
+    if(opacity.r < 0.5) {
+        discard;
+    }
 
     if(Layer == 2) {
-        FragColor = vec4(color.r,color.r,color.r,1.0);
+        FragColor = vec4(1.0,1.0,1.0,1.0);
     }else {
-        FragColor = color;
+        FragColor = texture(textureSampler, vec3(TexCoord,Layer));
     }
     //FragColor = vec4(vec3(factor), 1.0);
 }
