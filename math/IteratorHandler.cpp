b2c535050e448d2ccb74b39acb1cec041157e20b
Henrique Rocha <hdlopesrocha@protonmail.com>
2025-02-28 20:31:56 +0000

    Avoid sorting terminal nodes

--- math/IteratorHandler.cpp
+++ math/IteratorHandler.cpp
@@ -2,8 +2,8 @@
 
 void IteratorHandler::iterate(int level, OctreeNode * node, BoundingCube cube, void * context) {
     if(node != NULL) {
+        context = before(level,node, cube, context);
         if(test(level, node, cube, context)) {
-            context = before(level,node, cube, context);
             int internalOrder[8];
             getOrder(node, cube, internalOrder);
             for(int i=0; i <8 ; ++i) {
@@ -13,7 +13,7 @@ void IteratorHandler::iterate(int level, OctreeNode * node, BoundingCube cube, v
                     this->iterate(level+1, child, Octree::getChildCube(cube,j), context);
                 }
             }
-            after(level,node, cube, context);
         }
+        after(level,node, cube, context);
     }
 }
