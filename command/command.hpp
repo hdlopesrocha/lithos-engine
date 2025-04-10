9edb345d49a2dffa10ec0265f02fcf476159834c
Henrique Rocha <hdlopesrocha@protonmail.com>
2025-04-10 22:53:35 +0100

    Command as a template

--- command/command.hpp
+++ command/command.hpp
@@ -2,25 +2,10 @@
 #define COMMAND_HPP
 
 #include "../gl/gl.hpp"
-// ICommand Interface
-class ICommand {
+template<typename T> class ICommand {
     public:
     virtual ~ICommand() = default;
-    virtual void execute() = 0;
-};
-    
-// 1D analog input (e.g., trigger)
-class IAnalog1DCommand {
-    public:
-    virtual ~IAnalog1DCommand() = default;
-    virtual void execute(float value) = 0;
-};
-
-// 2D analog input (e.g., joystick)
-class IAnalog2DCommand {
-    public:
-    virtual ~IAnalog2DCommand() = default;
-    virtual void execute(const glm::vec2& input) = 0;
+    virtual void execute(const T& value) = 0;
 };
 
 // Controller Strategy Interface
