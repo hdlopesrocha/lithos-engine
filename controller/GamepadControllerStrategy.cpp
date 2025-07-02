432f685869b064625aeeb27dacee7a7361b6963f
Henrique Rocha <hdlopesrocha@protonmail.com>
2025-07-02 14:10:43 +0100

    [BROKEN] Fix gamepad rotation

--- controller/GamepadControllerStrategy.cpp
+++ controller/GamepadControllerStrategy.cpp
@@ -15,9 +15,9 @@ void GamepadControllerStrategy::handleInput(float deltaTime) {
         vector3d0.y = -state.axes[GLFW_GAMEPAD_AXIS_RIGHT_Y];
         vector3d0.z = state.axes[GLFW_GAMEPAD_AXIS_RIGHT_TRIGGER] - state.axes[GLFW_GAMEPAD_AXIS_LEFT_TRIGGER];
        
-        vector3d1.y = state.axes[GLFW_GAMEPAD_AXIS_LEFT_X];
-        vector3d1.x = state.axes[GLFW_GAMEPAD_AXIS_LEFT_Y];
-        vector3d1.z = (state.buttons[GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER] == GLFW_PRESS ? 1 : 0) - (state.buttons[GLFW_GAMEPAD_BUTTON_LEFT_BUMPER] == GLFW_PRESS ? 1 : 0);	
+        vector3d1.x = state.axes[GLFW_GAMEPAD_AXIS_LEFT_X];
+        vector3d1.z = -state.axes[GLFW_GAMEPAD_AXIS_LEFT_Y];
+        vector3d1.y = (state.buttons[GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER] == GLFW_PRESS ? 1 : 0) - (state.buttons[GLFW_GAMEPAD_BUTTON_LEFT_BUMPER] == GLFW_PRESS ? 1 : 0);	
     
         if(state.buttons[GLFW_GAMEPAD_BUTTON_DPAD_RIGHT] == GLFW_PRESS) {
             if(!keyWasPressed[GLFW_GAMEPAD_BUTTON_DPAD_RIGHT]) { 
@@ -42,10 +42,6 @@ void GamepadControllerStrategy::handleInput(float deltaTime) {
         }
     }
 
-
-
-
-
     float threshold = 0.2;
     vector3d0 = applyDeadzone(vector3d0, threshold);
     vector3d1 = applyDeadzone(vector3d1, threshold);
@@ -57,6 +53,5 @@ void GamepadControllerStrategy::handleInput(float deltaTime) {
     if(isAboveDeadzone(vector3d1, threshold)) {
         eventManager.publish<TimedAttribute<glm::vec3>>(EVENT_VECTOR_3D_1, TimedAttribute(deltaTime, vector3d1));
     }
-
 
 }
